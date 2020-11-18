a3e57a56b27d9f4fa49afd732385284a1ae38720
Oliix74 <70209183+Oliix74@users.noreply.github.com>
2020-11-18 12:07:27 +0100

    Add files via upload

---
+++ commands.js
@@ -0,0 +1,9 @@
+var commands = {
+
+    'salut': function() { alert('Bonjour!'); },
+    'bonjour': function() { alert('Salut!'); },
+    'salut': function() { alert('Salut! comment vas-tu ?'); },
+    'ça va': function() { alert('Bien et vous ?'); },
+    'est-ce que tu connais les NASCAR': function() { alert('Evidemment, j\'adore Denny Hamlin, Mon circuit préféré est Bristol');}
+
+}
